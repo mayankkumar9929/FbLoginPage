e0f9916975a6bda4fb2d01cd5ee1dcc5fe4d5f9a
Mayank Kumar <93407708+mayankkumar9929@users.noreply.github.com>
2021-10-30 02:34:46 +0530

    Created MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.mychatlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+     AppCompatButton reg;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        reg = findViewById(R.id.reg);
+
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistrationPage.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
